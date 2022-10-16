ec10131f68cd6e2f472c344cea449cf8ca48ec0a
ktakada42 <ktakada950711@gmail.com>
2022-10-16 21:44:25 +0900

    feat: create exit as utils

---
+++ includes/utils.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 21:14:41 by ktakada           #+#    #+#             */
+/*   Updated: 2022/10/16 21:14:41 by ktakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <stdlib.h>
+# include "../libft/includes/libft.h"
+
+void	exit_with_error(void);
+
+#endif //UTILS_H
