7d7e77046e7f4f169b12fdb82f081d65aaba3fb4
ktakada42 <ktakada950711@gmail.com>
2022-10-16 21:45:13 +0900

    feat: create parse_args.c/.h

---
+++ includes/parse_args.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 21:18:43 by ktakada           #+#    #+#             */
+/*   Updated: 2022/10/16 21:18:45 by ktakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_ARGS_H
+# define PARSE_ARGS_H
+
+# include <stdbool.h>
+# include <stddef.h>
+
+bool	is_args_valid(char **args);
+
+#endif //PARSE_ARGS_H
