15353d51e4fc6e8134b53844a7f53cc1eb663588
ktakada42 <ktakada950711@gmail.com>
2022-10-19 21:46:33 +0900

    feat: create swap operation

---
+++ includes/operation.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operation.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 21:17:47 by ktakada           #+#    #+#             */
+/*   Updated: 2022/10/19 21:51:25 by ktakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef OPERATION_H
+# define OPERATION_H
+
+# include "doubly_linked_list.h"
+
+void	swap(t_dll *list);
+
+#endif //OPERATION_H
---
+++ src/swap.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ktakada <ktakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/19 21:15:10 by ktakada           #+#    #+#             */
+/*   Updated: 2022/10/19 21:15:10 by ktakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/operation.h"
+
+void	swap(t_dll *list)
+{
+	t_dll	*first;
+	t_dll	*second;
+
+	if (list == NULL)
+		return ;
+	first = list->next;
+	if (first->next == list)
+		return ;
+	second = first->next;
+	first->next = second->next;
+	second->next->prev = first;
+	second->next = first;
+	first->prev = second;
+	list->next = second;
+	second->prev = list;
+}
+
+//#include <stdio.h>
+//
+//int	main(void)
+//{
+//	t_dll	*list;
+//	t_dll	*next;
+//	int 	i = 1;
+//
+//	list = new_doubly_linked_list();
+//	if (list == NULL)
+//		free_list_and_exit(list);
+//	swap(list);
+//	while (i < 3)
+//	{
+//		if (can_list_push_back(list, i))
+//			i++;
+//		else
+//			free_list_and_exit(list);
+//		if (i == 1)
+//			swap(list);
+//	}
+//	next = list->next;
+//	while (next != list)
+//	{
+//		printf("%d\n", next->value);
+//		next = next->next;
+//	}
+//	printf("///swap///\n");
+//	swap(list);
+//	next = list->next;
+//	while (next != list)
+//	{
+//		printf("%d\n", next->value);
+//		next = next->next;
+//	}
+//}
